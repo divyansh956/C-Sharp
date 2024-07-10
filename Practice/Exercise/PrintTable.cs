6ab5515249491a05b9ce78ec80fcaf3b8aeebfa7
Divyansh Saxena <divyanshsxn998@gmail.com>
2024-07-10 23:00:34 +0530

    Print table of a number

---
+++ Practice/Exercise/PrintTable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Exercise
+{
+    internal class PrintTable
+    {
+        void Main(string[] args)
+        {
+            bool success = true;
+            while (success)
+            {
+                Console.Write("Enter a number: ");
+                if (int.TryParse(Console.ReadLine(), out int num))
+                {
+                    success = false;
+                    for (int i = 1; i <= 10; i++)
+                    {
+                        Console.WriteLine($"{num} * {i} = {num * i}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid number");
+                }
+            }
+        }
+    }
+}
