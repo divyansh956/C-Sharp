5a16ae596cb6c85eec30a55fe7f83731dc5f34b9
Divyansh Saxena <divyanshsxn998@gmail.com>
2024-07-10 22:50:28 +0530

    TryParse

---
+++ Practice/TryParse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class TryParse
+    {
+        void Main(string[] args)
+        {
+            string numInput = Console.ReadLine();
+
+            bool success = int.TryParse(numInput, out int num);
+            if (!success)
+            {
+                Console.WriteLine("Invalid number");
+                return;
+            }
+
+            Console.WriteLine(num);
+        }
+    }
+}
