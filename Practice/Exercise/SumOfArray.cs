680aa2eb64c31d8731422b4aece9b2b34e2976a4
Divyansh Saxena <divyanshsxn998@gmail.com>
2024-07-11 13:05:25 +0530

    Functions

---
+++ Practice/Exercise/SumOfArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Exercise
+{
+    internal class SumOfArray
+    {
+        void Main()
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+            int sum = SumOfNumbers(numbers);
+            Console.WriteLine(sum);
+        }
+
+        static int SumOfNumbers(int[] numbers)
+        {
+            if (numbers.Length == 0)
+            {
+                return -1;
+            }
+
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Practice/Functions.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Test
+    {
+        public static void TestFunction()
+        {
+            Console.WriteLine("Test Function");
+        }
+    }
+    internal class Functions
+    {
+        void Main()
+        {
+            PrintHelloWorld();
+            Test.TestFunction();
+
+            bool success = int.TryParse("123", out int result); // TryParse returns true if the parsing was successful
+            if (success)
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Failed to parse");
+            }
+
+            int sum = Add(5, 10);
+            Console.WriteLine(sum);
+
+            string nameInput = "John";
+            int ageInput = 25;
+            PrintNameAndAge(age: ageInput, name: nameInput); // Named arguments
+
+            bool changed = changeNumber(out int num); // out argument
+            Console.WriteLine(num);
+
+            int n = 19;
+            changeNum(ref n); // reference argument
+            Console.WriteLine(n);
+        }
+
+        static void changeNum(ref int num)
+        {
+            num = 10;
+        }
+
+        static bool changeNumber(out int num)
+        {
+            num = 5;
+            return true;
+        }
+
+        static void PrintNameAndAge(string name, int age)
+        {
+            Console.WriteLine($"Name: {name}, Age: {age}");
+        }
+
+        static void PrintHelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int Add(int a, [Optional] int b)
+        {
+            return a + b;
+        }
+    }
+
+}
