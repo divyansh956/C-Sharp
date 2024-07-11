30822389ab463b8cf1bd8f10733c3217a7611335
Divyansh Saxena <divyanshsxn998@gmail.com>
2024-07-11 08:47:25 +0530

    Password Checker

---
+++ Practice/Exercise/PasswordChecker.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Exercise
+{
+    internal class PasswordChecker
+    {
+        static bool ValidatePassword(string password)
+        {
+            if (password.Length < 8)
+            {
+                Console.WriteLine("Password must be at least 8 characters long.");
+                return false;
+            }
+
+            if (!password.Any(char.IsUpper))
+            {
+                Console.WriteLine("Password must contain at least one uppercase letter.");
+                return false;
+            }
+
+            if (!password.Any(char.IsLower))
+            {
+                Console.WriteLine("Password must contain at least one lowercase letter.");
+                return false;
+            }
+
+            if (!password.Any(char.IsDigit))
+            {
+                Console.WriteLine("Password must contain at least one digit.");
+                return false;
+            }
+
+            if (!password.Any(ch => !char.IsLetterOrDigit(ch)))
+            {
+                Console.WriteLine("Password must contain at least one special character.");
+                return false;
+            }
+
+            return true;
+        }
+
+        void Main(string[] args)
+        {
+            Console.Write("Enter password: ");
+            string s = Console.ReadLine();
+            bool result = ValidatePassword(s);
+            Console.WriteLine(result);
+        }
+    }
+}
