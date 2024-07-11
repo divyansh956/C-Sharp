38eaab3fb1d7c3ff12d83c9c6ad07e9f2bb99283
Divyansh Saxena <divyanshsxn998@gmail.com>
2024-07-11 08:39:13 +0530

    String

---
+++ Practice/Exercise/ReverseString.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Exercise
+{
+    internal class ReverseString
+    {
+        void Main(string[] args)
+        {
+            string message = "C# is a programming language";
+            var reversedMessage = new string(message.Reverse().ToArray());
+            Console.WriteLine(reversedMessage);
+
+            // Another way to reverse a string
+            char[] chars = message.ToCharArray();
+            Array.Reverse(chars);
+            reversedMessage = new string(chars);
+            Console.WriteLine(reversedMessage);
+
+            // Another way to reverse a string
+            reversedMessage = string.Empty;
+            for (int i = message.Length - 1; i >= 0; i--)
+            {
+                reversedMessage += message[i];
+            }
+            Console.WriteLine(reversedMessage);
+        }
+    }
+}
---
+++ Practice/String.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class String
+    {
+        void Main(string[] args)
+        {
+            string path = "C:\\Users\\Desktop\\C#";
+            path = @"C:\Users\Desktop\C#"; // Verbatim string literal
+            string speech = "Hello, my name is \"John\".";
+            Console.WriteLine(path);
+            Console.WriteLine(speech);
+
+            string s = @"Hello ""John""";
+            s = @"Hello 'John'";
+            Console.WriteLine(s);
+
+            string name = "John";
+            int age = 30;
+            string message = string.Format("My name is {0} and I am {1} years old.", name, age);
+            Console.WriteLine(message);
+
+            string test = string.Concat("Hello", " ", "World");
+            Console.WriteLine(test);
+
+            Console.WriteLine("My name is {0} and I am {1} years old.", name, age); // Composite formatting
+            Console.WriteLine($"My name is {name} and I am {age} years old."); // String interpolation
+
+            Console.WriteLine("" == string.Empty || string.IsNullOrEmpty("")); // True
+            Console.WriteLine("Hello".Equals("Hello"));
+
+            char[] chars = new char[] { 'H', 'e', 'l', 'l', 'o' };
+            object s1 = new string(chars);
+            Console.WriteLine("Hello" == s1); // False (Reference equality)
+
+            message = "C# is a programming language";
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.Write(message[i]);
+                Thread.Sleep(100);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(message.Substring(4, 3));
+            Console.WriteLine(message.IndexOf("programming"));
+            Console.WriteLine("Message".Contains("e"));
+            string[] words = message.Split(' ');
+            foreach (var word in words)
+            {
+                Console.Write(word);
+            }
+        }
+    }
+}
